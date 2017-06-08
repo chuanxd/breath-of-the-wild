bdefb919ea6e4be66cff2c245aa9f9ac83abaf8e
Timmy Chang <timmy5519@gmail.com>
2017-06-09 02:29:20 +0800

    init

---
+++ src/containers/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-wrapper">
+    @home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+
+</script>
+
+<style lang="scss" scoped>
+.home-wrapper {
+  background: red;
+}
+</style>
