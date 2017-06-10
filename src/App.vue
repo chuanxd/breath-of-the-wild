03bb2b0fa421090f51ea881f8316e272923bba16
Timmy Chang <timmy5519@gmail.com>
2017-06-10 13:15:07 +0800

    Add html body style

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  background: #0e1626;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
