712569679ccc0fcc4d5243200c7692a2cfcccb7a
Timmy Chang <timmy5519@gmail.com>
2017-06-10 13:23:01 +0800

    Set `vue2-google-map` config   - add google map API token

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.mapToken
+  }
+  // Demonstrating how we can customize the name of the components
+  // installComponents: false
+})
 
 /* eslint-disable no-new */
 new Vue({
